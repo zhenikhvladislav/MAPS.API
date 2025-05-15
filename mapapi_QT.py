0dc0496f6114a698401a2834f72021478081ffb0
zhenikhvladislav <zhenikhvladislav@gmail.com>
2025-05-15 14:12:21 +0300

    Сделал 2 задачу. Добавлена обработка клавиш PgUP и PgDN

--- mapapi_QT.py
+++ mapapi_QT.py
@@ -8,15 +8,17 @@ import os
 class MapApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Яндекс.Карты')
+        self.setWindowTitle('Яндекс.Карты с масштабированием')
         self.setGeometry(100, 100, 650, 450)
 
-        # Центр Москвы (точные координаты)
+        # Параметры карты
         self.ll = "37.617635,55.755814"  # Координаты Красной площади
-        self.spn = "0.05,0.05"  # Оптимальный масштаб для центра города
+        self.spn = 0.05  # Начальный масштаб
+        self.min_spn = 0.001  # Минимальный масштаб
+        self.max_spn = 90.0   # Максимальный масштаб (весь мир)
         self.size = '650,450'
 
-        # Создаем центральный виджет для точного позиционирования
+        # Создаем центральный виджет
         self.central_widget = QLabel()
         self.central_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.setCentralWidget(self.central_widget)
@@ -24,9 +26,8 @@ class MapApp(QMainWindow):
         self.load_map()
 
     def load_map(self):
-        static_server_address = 'https://static-maps.yandex.ru/1.x/?'
         try:
-            url = f'{static_server_address}ll={self.ll}&spn={self.spn}&size={self.size}&l=map'
+            url = f'https://static-maps.yandex.ru/1.x/?ll={self.ll}&spn={self.spn},{self.spn}&size={self.size}&l=map'
             response = requests.get(url)
             response.raise_for_status()
 
@@ -40,6 +41,18 @@ class MapApp(QMainWindow):
 
         except Exception as e:
             print(f'Ошибка при загрузке карты: {e}')
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp:
+            # Увеличение масштаба (приближение)
+            self.spn = max(self.spn / 1.5, self.min_spn)
+            self.load_map()
+        elif event.key() == Qt.Key.Key_PageDown:
+            # Уменьшение масштаба (отдаление)
+            self.spn = min(self.spn * 1.5, self.max_spn)
+            self.load_map()
+        else:
+            super().keyPressEvent(event)
 
     def closeEvent(self, event):
         if os.path.exists('temp_map.png'):
